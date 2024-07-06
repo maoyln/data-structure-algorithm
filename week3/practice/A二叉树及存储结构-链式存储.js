60afaaa95faf297634a35970cffc7da1e1968318
maoyln <maoyl_yx@163.com>
2024-07-06 09:03:45 +0800

    二叉树存储结构

---
+++ week3/practice/A二叉树及存储结构-链式存储.js
@@ -0,0 +1,20 @@
+class TreeNode {
+  constructor(value) {
+    this.value = value; // 节点数据
+    this.left = null;   // 左子节点指针
+    this.right = null;  // 右子节点指针
+  }
+}
+
+// 创建根节点
+const root = new TreeNode('root');
+
+// 创建子节点
+const leftChild = new TreeNode('left');
+const rightChild = new TreeNode('right');
+
+// 连接子节点
+root.left = leftChild;
+root.right = rightChild;
+
+console.log(root);
---
+++ week3/practice/B二叉树及存储结构-顺序存储结构.js
@@ -0,0 +1,14 @@
+// 使用数组表示二叉树
+const tree = ['root', 'left', 'right', 'left.left', 'left.right', 'right.left', 'right.right'];
+
+// 访问根节点
+console.log(tree[0]); // 'root'
+
+// 访问左子节点
+console.log(tree[1]); // 'left'
+
+// 访问右子节点
+console.log(tree[2]); // 'right'
+
+// 访问左子节点的左子节点
+console.log(tree[3]); // 'left.left'
